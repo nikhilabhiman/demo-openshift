a0095ec7dc7f973067387abd05bd739038e2773d
nikhilabhiman <nikhil.abhiman@techolution.com>
2018-08-04 11:29:34 -0400

    Added files

---
+++ src/main/java/com/test/openshift/demoopenshift/DemoOpenshiftApplication.java
@@ -0,0 +1,12 @@
+package com.test.openshift.demoopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOpenshiftApplication.class, args);
+	}
+}
